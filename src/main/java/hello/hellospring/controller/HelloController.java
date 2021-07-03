65bf72ea4d3d18f9ba8a64151a8f5ea5ef2c6299
Song-EunJu <jd06280@naver.com>
2021-07-03 20:59:57 +0900

    [2] 스프링 웹 개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,41 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value= "name") String name, Model model){
+        model.addAttribute("name",name); // 앞의 name이 key, 뒤의 name은 parameter로 넘어온 값
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // http body 부분에 이 데이터("hello" + name)를 직접 넣어주겠다. name 을 spring 으로 넣으면 hello spring이 리턴.
+    // 템플릿 엔진과의 차이 : view 가 없고 이 문자가 그대로 내려감. html 태그 없이 문자만 그대로 내려감
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("p") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // json으로 반환
+    }
+
+    static class Hello {
+        private String abc;
+
+
+        public String getName() {
+            return abc;
+        }
+
+        public void setName(String abc) {
+            this.abc = abc;
+        }
     }
 }
 
