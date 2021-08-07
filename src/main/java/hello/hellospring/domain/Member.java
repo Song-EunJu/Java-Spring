2a08335d0c3392371dd6d76abe47d30e82191b48
Song-EunJu <jd06280@naver.com>
2021-08-07 21:04:02 +0900

    [ADD] JPA repository 생성

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,17 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    // db가 알아서 생성해주는 것은 identity = 아까 id 번호
     private Long id; // 시스템에 저장할 때 시스템이 정하는 id
+
     private String name; // 고객이 회원가입할 때 입력하는 이름
 
     public Long getId() {
